8b89522fe46f280b9d7cb2cd1021520512fe4dc1
Your name <you@example.com>
2026-06-11 18:54:44 +0300

    Добавить фильтрацию с useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import FilmCard from './components/FilmCard.jsx';
+import './App.css';
 
-const allFilms = [
+const allFilmsData = [
   {
     id: 1,
     title: 'Титаник',
@@ -9,6 +10,8 @@ const allFilms = [
     genre: 'Драма',
     likesCount: 30,
     dislikesCount: 12,
+    liked: false,
+    disliked: false
   },
   {
     id: 2,
@@ -17,6 +20,8 @@ const allFilms = [
     genre: 'Мелодрама',
     likesCount: 42,
     dislikesCount: 31,
+    liked: false,
+    disliked: false
   },
   {
     id: 3,
@@ -25,6 +30,8 @@ const allFilms = [
     genre: 'Триллер',
     likesCount: 49,
     dislikesCount: 15,
+    liked: false,
+    disliked: false
   },
   {
     id: 4,
@@ -33,124 +40,201 @@ const allFilms = [
     genre: 'Боевик',
     likesCount: 52,
     dislikesCount: 36,
+    liked: false,
+    disliked: false
   },
 ];
 
 function App() {
-  const [films, setFilms] = useState(allFilms);
-  const [isLiked, setisLiked] = useState([]);
-  const [disLiked, setdisLiked] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [allFilms, setAllFilms] = useState([])
+  const [liked, setLiked] = useState([]);
+  const [disLiked, setDisLiked] = useState([]);
+  const [viewedFilms, setViewedFilms] = useState([]);
   const [viewCount, setViewCount] = useState(0);
 
+
+  const [filterName, setFilterName] = useState('');
+  const [filterYearFrom, setFilterYearFrom] = useState('');
+  const [filterYearTo, setFilterYearTo] = useState('');
+  const [filterGenre, setFilterGenre] = useState('');
+
   useEffect(() => {
-    const connection = {
-      init: () => {
-        const viewedFilmsCount = new Set([...isLiked, ...disLiked]).size;
-        setViewCount(viewedFilmsCount);
-      },
-      disconnect: () => {
+    setAllFilms(allFilmsData);
+    setFilms(allFilmsData);
+  }, []);
+
+  useEffect(() => {
+    setViewCount(viewedFilms.length);
+  }, [viewedFilms]);
+
+  const addToViewed = (id) => {
+    setViewedFilms(prev => {
+      if (!prev.includes(id)) {
+        return [...prev, id];
       }
-    };
-    connection.init();
-    return () => {
-      connection.disconnect();
-    };
-  }, [isLiked, disLiked]);
+      return prev;
+    });
+  };
+
+  useEffect(() => {
+    let filtered = [...allFilms];
+
+    if (filterName) {
+      filtered = filtered.filter(f => f.title.toLowerCase().includes(filterName.toLowerCase()));
+    }
+    if (filterGenre) {
+      filtered = filtered.filter(f => f.genre === filterGenre);
+    }
+    if (filterYearFrom) {
+      filtered = filtered.filter(f => f.year >= parseInt(filterYearFrom));
+    }
+    if (filterYearTo) {
+      filtered = filtered.filter(f => f.year <= parseInt(filterYearTo));
+    }
+
+    setFilms(filtered);
+  }, [filterName, filterYearFrom, filterYearTo, filterGenre, allFilms]);
 
   const handleLike = (id) => {
     setFilms(prev => prev.map(f => {
       if (f.id === id) {
-        if (isLiked.includes(id)) {
-          setisLiked(prevLiked => prevLiked.filter(item => item !== id));
-          return { ...f, likesCount: f.likesCount - 1 };
+        if (f.liked) {
+          return {...f, liked: false, likesCount: f.likesCount - 1};
         } else {
-          setisLiked(prevLiked => [...prevLiked, id]);
-          if (disLiked.includes(id)) {
-            setdisLiked(prevDisliked => prevDisliked.filter(item => item !== id));
-            return {
-              ...f,
-              likesCount: f.likesCount + 1,
-              dislikesCount: f.dislikesCount - 1
-            };
+            let newLikesCount = f.likesCount + 1;
+            let newDislikesCount = f.dislikesCount;
+            if (f.disliked) {
+              newDislikesCount = f.dislikesCount - 1;
+            }
+            return {...f,liked: true, disliked: false, likesCount: newLikesCount, dislikesCount: newDislikesCount};
           }
-          return { ...f, likesCount: f.likesCount + 1 };
-        }
       }
       return f;
     }));
+
+    setLiked(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+    setDisLiked(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id);
+      } else {
+        return prev.filter(item => item !== id);
+      }
+    });
   };
 
   const handleDislike = (id) => {
     setFilms(prev => prev.map(f => {
       if (f.id === id) {
-        if (disLiked.includes(id)) {
-          setdisLiked(prevDisliked => prevDisliked.filter(item => item !== id));
-          return { ...f, dislikesCount: f.dislikesCount - 1 };
+        if (f.disliked) {
+          return {...f, disliked: false, dislikesCount: f.dislikesCount - 1};
         } else {
-          setdisLiked(prevDisliked => [...prevDisliked, id]);
-          if (isLiked.includes(id)) {
-            setisLiked(prevLiked => prevLiked.filter(item => item !== id));
-            return {
-              ...f,
-              dislikesCount: f.dislikesCount + 1,
-              likesCount: f.likesCount - 1
-            };
+            let newDislikesCount = f.dislikesCount + 1;
+            let newLikesCount = f.likesCount;
+            if (f.liked) {
+              newLikesCount = f.likesCount - 1;
+            }
+            return {...f, disliked: true, liked: false, dislikesCount: newDislikesCount, likesCount: newLikesCount};
           }
-          return { ...f, dislikesCount: f.dislikesCount + 1 };
-        }
       }
       return f;
     }));
+    
+    setDisLiked(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id);
+      } else {
+        return [...prev, id];
+      }
+    });
+
+    setLiked(prev => {
+      if (prev.includes(id)) {
+        return prev.filter(item => item !== id);
+      } else {
+        return prev.filter(item => item !== id);
+      }
+    });
   };
 
-  const likedFilms = films.filter(f => isLiked.includes(f.id));
-  const dislikedFilms = films.filter(f => disLiked.includes(f.id));
+  const likedFilms = films.filter(f => f.liked);
+  const dislikedFilms = films.filter(f => f.disliked);
 
   return (
-    <div style={{ position: 'relative', display: 'flex', padding: '20px', gap: '20px', fontFamily: 'Arial' }}>
-      <div style={{
-        position: 'absolute',
-        bottom: '10px',
-        right: '10px',
-        backgroundColor: '#fff',
-        padding: '8px 12px',
-        border: '1px solid #ccc',
-        borderRadius: '8px',
-        fontWeight: 'bold'
-      }}>
-        Просмотрено: {viewCount}
+    <div className="container">
+      <div className="filters">
+        <input
+          type="text"
+          placeholder="Название"
+          value={filterName}
+          onChange={(e) => setFilterName(e.target.value)}/>
+        <select
+          value={filterGenre}
+          onChange={(e) => setFilterGenre(e.target.value)}>
+          <option value="">Все жанры</option>
+          <option value="Драма">Драма</option>
+          <option value="Мелодрама">Мелодрама</option>
+          <option value="Триллер">Триллер</option>
+          <option value="Боевик">Боевик</option>
+        </select>
+        <input
+          type="number"
+          placeholder="Год от"
+          value={filterYearFrom}
+          onChange={(e) => setFilterYearFrom(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Год до"
+          value={filterYearTo}
+          onChange={(e) => setFilterYearTo(e.target.value)}
+        />
       </div>
 
-      <div style={{ flex: 3 }}>
-        <h2>Каталог фильмов</h2>
-        {films.map((film) => (
-          <FilmCard
-            key={film.id}
-            title={film.title}
-            date={film.year}
-            genre={film.genre}
-            likes={film.likesCount}
-            dislikes={film.dislikesCount}
-            handleLike={() => handleLike(film.id)}
-            handleDislike={() => handleDislike(film.id)}
-            isLiked={isLiked.includes(film.id)}
-            isDisliked={disLiked.includes(film.id)}
-          />
-        ))}
+      <div className="viewCounter">
+        Просмотрено: {viewCount}
       </div>
-      <div style={{ flex: 1 }}>
-        <div>
-          <h3>Мне понравилось ({likedFilms.length})</h3>
-          {likedFilms.map(f => (
-            <div key={f.id}>{f.title}</div>
+      <div className="mainContent">
+        <div className="filmList">
+          <h2>Каталог фильмов</h2>
+          {films.map((film) => (
+            <div key={film.id} onClick={() => addToViewed(film.id)}>
+              <FilmCard
+                title={film.title}
+                date={film.year}
+                genre={film.genre}
+                likes={film.likesCount}
+                dislikes={film.dislikesCount}
+                handleLike={() => handleLike(film.id)}
+                handleDislike={() => handleDislike(film.id)}
+                isLiked={film.liked}
+                isDisliked={film.disliked}
+              />
+            </div>
           ))}
         </div>
-        <div style={{ marginTop: '20px' }}>
-          <h3>Мне не понравилось ({dislikedFilms.length})</h3>
-          {dislikedFilms.map(f => (
-            <div key={f.id}>{f.title}</div>
-          ))}
+
+        <div className="likeDisliked">
+          <div>
+            <h3>Мне понравилось ({likedFilms.length})</h3>
+            {likedFilms.map(f => (
+              <div key={f.id}>{f.title}</div>
+            ))}
+          </div>
+          <div style={{ marginTop: '20px'}}>
+            <h3>Мне не понравилось ({dislikedFilms.length})</h3>
+            {dislikedFilms.map(f => (
+              <div key={f.id}>{f.title}</div>
+            ))}
+          </div>
         </div>
+        
       </div>
     </div>
   );
